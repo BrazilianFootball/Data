d7298d01c0d1afad218c12c5af31e5472f17a67c
IgorMichels <igorxadrezbr@gmail.com>
2023-08-02 22:22:52 -0300

    update test

--- scripts/test.py
+++ scripts/test.py
@@ -17,7 +17,8 @@ def get_public_ip():
 
 def test_docket():
     try:
-        response = requests.get('https://conteudo.cbf.com.br/sumulas/2023/1421se.pdf')
+        response = requests.get('https://conteudo.cbf.com.br/sumulas/2023/1421se.pdf',
+                                verify = False)
         if response.status_code == 200:
             data = response.content
             return data
